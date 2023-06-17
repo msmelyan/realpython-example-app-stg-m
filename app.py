ae08e3bb2c7d0111924f0888689435621f000f38
Your Name <you@example.com>
2023-06-17 11:37:32 -0700

    yet another change 2

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    value = request.args.get('value', 'No Value Provided')
-    return f'Hello World: {value}'
+    value1 = request.args.get('value1', 'No Value Provided')
+    value2 = request.args.get('value2', 'No Value Provided')
+    return f'Hello World: value1={value1} value2={value2}'
